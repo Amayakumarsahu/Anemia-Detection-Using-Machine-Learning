f283cc763379a6c18853854e4189abcfe5f1e3a7
Amaya Kumar Sahu <amayakumarsahu9@gmail.com>
2025-07-15 19:54:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 app = Flask(__name__)
 
 # Load the trained model
-model = pickle.load(open('c:\\Users\\Amaya\\OneDrive\\Desktop\\Anemia detection project\\model.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
